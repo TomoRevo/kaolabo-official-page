6d754b7f992a38946f242ffb648172881c7c81a3
TomoRevo <tomorevo.inc@gmail.com>
2025-11-09 09:15:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ document.querySelector('#app').innerHTML = `
 
   <section class="section what-is">
     <div class="container">
-      <h2 class="section-title">顔ヨガとは</h2>
+      <h2 class="section-title">顔ヨガって？</h2>
       <div class="what-is-content">
         <p class="what-is-definition">顔ヨガとは、お顔の<span class="highlight-text">表情筋を鍛えてストレッチ</span>することです。</p>
 
@@ -455,7 +455,7 @@ document.querySelector('#app').innerHTML = `
           自分らしく生きられる。<br><br>
           そんな人生を、一緒に歩みませんか？
         </p>
-        <button class="cta-button" onclick="alert('お申込みフォームへ遷移します')">顔laboに参加する</button>
+        <a href="https://univa.cc/7aUFJV" target="_blank" class="cta-button">顔laboに参加する</a>
         
       </div>
     </div>
